5495742c14f086cda33951256d078afb15e2379d
Andy Brown <asbrown002@gmail.com>
2016-08-10 10:20:24 -0700

    decrease base font size

--- init.el
+++ init.el
@@ -149,7 +149,7 @@ values."
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
    dotspacemacs-default-font '("Inconsolatag"
-                               :size 16
+                               :size 14
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
